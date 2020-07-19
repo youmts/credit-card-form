896316fbf560daebf432934c1c060c997a6daa40
You Matsumoto <youmts22@gmail.com>
2020-07-19 09:04:03 +0900

    feat: add kanban

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Kanban msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Kanban from "./components/Kanban.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld
+    Kanban
   }
 });
 </script>
---
+++ src/components/Kanban.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    カンバン
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+});
+</script>
+
+<style scoped lang="scss">
+</style>
