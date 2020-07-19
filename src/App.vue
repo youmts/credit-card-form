16c687f6559c86dd6236128646c3e71e549187cf
You Matsumoto <youmts22@gmail.com>
2020-07-19 09:23:59 +0900

    feat: card

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <Kanban msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <b-nav>
+      <b-nav-item>Like Github Project</b-nav-item>
+    </b-nav>
+    <b-container>
+      <Project />
+    </b-container>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Kanban from "./components/Kanban.vue";
+import Project from "./components/Project.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
-    Kanban
+    Project
   }
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
